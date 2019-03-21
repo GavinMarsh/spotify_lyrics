0f57bc52d9c3a385db22456db188d1757a0239c2
Son Nguyen Dang <dangson302@gmail.com>
2019-03-21 10:53:25 +0700

    Remove some global variable and change some code flows

--- __main__.py
+++ __main__.py
@@ -3,31 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import time
+import lyricsgenius
 
-query = ''
-currentSong = ''
-TOKEN = ''
+SPOTIFY_TOKEN = 'BQBVbtf-dq1t9CiaHwCDwZtwFCAVCKGTPHOvabfP-F9-6H3frP6FsbCTTWUxgoDVX4b9e8tKpmmlmH69HrAYU4hpawsiWrdQnXogU4cfVGlMteqSW7ozWJc_URwfvR3qg6diBXCkky9B9XRk7l2Geaj1C9qRZy1gQUlWrtg'
 # Get oauth token from https://developer.spotify.com/console/get-users-currently-playing-track
-
-def song_data():
-    global query
-    headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + TOKEN,
-    }
-
-    response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
-    json_data = json.loads(response.text)
-    try:
-        ARTIST = json_data["item"]["artists"][0]["name"]
-        SONG = json_data["item"]["name"]
-        query = SONG + " " + ARTIST + " +lyrics"
-        return 'Artist: %s, Song: %s' % (ARTIST, SONG)
-    except KeyError:
-        return currentSong
-
-
 
 headers_Get = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
@@ -39,31 +18,44 @@ headers_Get = {
         'Upgrade-Insecure-Requests': '1'
     }
 
+def song_data():
+    headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + SPOTIFY_TOKEN,
+    }
 
-def get_Song_Lyrics(query):
+    response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
+    json_data = json.loads(response.text)
+    try:
+        ARTIST = json_data["item"]["artists"][0]["name"]
+        SONG = json_data["item"]["name"]
+        return ARTIST, SONG
+    except KeyError:
+        return None
+
+def get_Song_Lyrics(song, artist):
+    query = song + " " + artist + " +lyrics"
     minestrone = '\n'
     s = requests.Session()
     query = '+'.join(query.split())
     url = 'https://www.google.com/search?q=' + query + '&ie=utf-8&oe=utf-8'
+    print("Googling:", url)
     r = s.get(url, headers=headers_Get)
     soup = BeautifulSoup(r.text, "html.parser").find_all("span", {"jsname": "YS01Ge"})
     for link in soup:
         minestrone += (link.text + '\n')
     return minestrone
 
-
 def main():
-    global query
-    print(song_data())
-    currentSong = song_data()
-    print(get_Song_Lyrics(query))
     while True:
-        if song_data() != currentSong:
-            print(song_data())
-            print(get_Song_Lyrics(query))
-            currentSong = song_data()
-        time.sleep(1)
-
+        currentSong = song_data()
+        if currentSong:
+            print("Song: {}, artist: {}".format(currentSong[0], currentSong[1]))
+            print(get_Song_Lyrics(*currentSong))
+        else:
+            print("Could not get song data")
+        time.sleep(1000)
 
 if __name__ == '__main__':
     try:
